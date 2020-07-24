b7a9c22dfbfb9f855aecefef409490f3afa972a0
Lehman Garrison <lgarrison@flatironinstitute.org>
2020-07-24 17:17:45 -0400

    Work on travis

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,9 +6,8 @@ PARTICLES_OUTPUT = os.path.join(os.path.dirname(__file__), 'particles.txt')
 import tempfile
 import filecmp
 
-def test_data_compaso_halo_catalog(tmp_path):
-    '''
-    Test abacusnbody.data.compaso_halo_catalog
+def test_halos(tmp_path):
+    '''Test loading a halo catalog
     '''
 
     from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
@@ -21,11 +20,37 @@ def test_data_compaso_halo_catalog(tmp_path):
 
     assert filecmp.cmp(HALOS_OUTPUT,tmp_path/'halos_test.txt')
 
+
+def test_subsamples(tmp_path):
+    '''Test loading particle subsamples
+    '''
+
+    from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
+
+    cat = CompaSOHaloCatalog(os.path.join(EXAMPLE_SIM, 'halos', 'z0.000'), load_subsamples=True, fields='all')
+
     with open(tmp_path/'particles_test.txt', 'w') as fp:
         f = cat.subsamples[::50].pformat_all()
         fp.write('\n'.join(f))
 
     assert filecmp.cmp(PARTICLES_OUTPUT,tmp_path/'particles_test.txt')
 
+
+def test_field_subset_loading():
+    '''Test loading a subset of halo catalog columns
+    '''
+    from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
+
     cat = CompaSOHaloCatalog(os.path.join(EXAMPLE_SIM, 'halos', 'z0.000'), fields=['N','x_com'])
-    assert(set(cat.halos.colnames) == set(['N','x_com']))
+    assert set(cat.halos.colnames) == set(['N','x_com'])
+
+
+def test_one_halo_info():
+    '''Test loading a single halo_info file
+    '''
+    from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
+
+    cat = CompaSOHaloCatalog(os.path.join(EXAMPLE_SIM, 'halos', 'z0.000', 'halo_info', 'halo_info_000.asdf'),
+        load_subsamples=True)
+    assert len(cat.halos) == 127
+    assert len(cat.subsamples) == 9306
---
+++ tests/test_pipe_asdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# fail on error
+set -e
+
+# change to tests/
+cd $(dirname $0)
+
+# run pipe_asdf
+TMPFN=$(mktemp)
+pipe_asdf Mini_N64_L32/halos/z0.000/halo_info/halo_info_*.asdf -f N -f x_com | ../pipe_asdf/client > $TMPFN
+
+# compare to known output
+diff $TMPFN ./pipe.txt
