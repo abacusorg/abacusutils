Re-enable Numba parallelism in environmental calculation now that Numba is fixed

abacusnbody/hod/prepare_sim.py
@@ -217,7 +217,7 @@ def concat_to_arr(lists, dtype=np.int64):
     res = np.fromiter(itertools.chain.from_iterable(lists), count=N, dtype=dtype)
     return res, starts
 
-@njit(parallel=False)
+@njit(parallel=True)
 def calc_Menv(masses, inner_arr, inner_starts, outer_arr, outer_starts):
     N = len(inner_starts)-1
     Menv = np.zeros(N, dtype=np.float32)
@@ -233,7 +233,7 @@ def calc_Menv(masses, inner_arr, inner_starts, outer_arr, outer_starts):
         Menv[p] = outer_mass - inner_mass
     return Menv
 
-@njit(parallel=False)
+@njit(parallel=True)
 def calc_fenv_opt(Menv, mbins, halosM):
     fenv_rank = np.zeros(len(Menv))
     for ibin in numba.prange(len(mbins)-1):