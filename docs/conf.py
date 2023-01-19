a990fe33d433ccd4ecbdb06696d78d751dd8b831
Lehman Garrison <lgarrison@flatironinstitute.org>
2023-01-19 14:08:02 -0500

    try removing readthedocs_ext

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'Daniel Eisenstein, Philip Pinto, Lehman Garrison, Nina Maksimova, Sown
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['readthedocs_ext.readthedocs',
-              'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.intersphinx',
               'sphinx.ext.autosectionlabel',
