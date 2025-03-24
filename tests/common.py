daf6acab7bde67c741d89969bf5b57b179c19247
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-03-24 13:28:09 -0400

    tests: deal with structured arrays inside assert_close
    This lets us give informative error messages that include the field name,
    and reduces repetition.

--- tests/common.py
+++ tests/common.py
@@ -9,6 +9,16 @@ import numpy.testing as npt
 
 def assert_close(arr1, arr2):
     """Checks exact equality for int arrays, and np.isclose for floats"""
+    if arr1.dtype.names is not None:
+        assert set(arr1.dtype.names) == set(arr2.dtype.names)
+        # if the arrays are structured, check each field
+        for name in arr1.dtype.names:
+            try:
+                assert_close(arr1[name], arr2[name])
+            except AssertionError as e:
+                raise AssertionError(f'Field "{name}" does not match') from e
+        return
+
     if issubclass(arr1.dtype.type, numbers.Integral):
         assert issubclass(arr2.dtype.type, numbers.Integral)
         npt.assert_array_equal(arr1, arr2)
